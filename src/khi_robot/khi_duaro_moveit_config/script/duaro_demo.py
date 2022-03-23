febb762f072cf2e6d354cc71331ba29883707703
AndreiVoica <andreivoica24@gmail.com>
2022-03-23 14:30:14 +0100

    Start of skills

--- src/khi_robot/khi_duaro_moveit_config/script/duaro_demo.py
+++ src/khi_robot/khi_duaro_moveit_config/script/duaro_demo.py
@@ -11,42 +11,25 @@ if __name__ == '__main__':
     sleep_time = 0.2
     exec_vel = 1.0
 
-    while True:
-        # home
+    def position_one():
         group.set_max_velocity_scaling_factor(exec_vel*0.2)
-        group.set_joint_value_target([-0.78, 0.78, 0.14, 0.0, 0.78, -0.78, 0.14, 0.0])
+        group.set_joint_value_target([-0.78, 0.78, 0.14, 0.0, 0.78, -0.78, 0.14, 0.0]) #[-0.78, 0.78, 0.14, 0.0, 0.78, -0.78, 0.14, 0.0]
         group.go()
         rospy.sleep(sleep_time)
 
+    def position_two():
         group.set_max_velocity_scaling_factor(exec_vel)
-        group.set_joint_value_target([-1.5, 1.5, 0.14, 0.0, 1.5, -1.5, 0.14, 0.0])
+        group.set_joint_value_target([-1.5, 1.5, 0.14, 0.0, 1.5, -1.5, 0.14, 0.0]) #[-1.5, 1.5, 0.14, 0.0, 1.5, -1.5, 0.14, 0.0]
         group.go()
         rospy.sleep(sleep_time)
 
-        num = 0
-        while num < 5:
-            group.set_max_velocity_scaling_factor(exec_vel*0.2)
-            group.set_joint_value_target([-1.5, 1.5, 0.06, 0.0, 1.5, -1.5, 0.06, 0.0])
-            group.go()
-            group.set_max_velocity_scaling_factor(exec_vel*0.2)
-            group.set_joint_value_target([-1.5, 1.5, 0.14, 0.0, 1.5, -1.5, 0.14, 0.0])
-            group.go()
-            num += 1
-
-        #group.set_max_velocity_scaling_factor(exec_vel)
-        #group.set_joint_value_target([-0.6, 0.78, 0.0, 0.0, 0.6, -0.78, 0.0, 0.0])
-        #group.go()
-        #rospy.sleep(sleep_time)
-
+    def lower_pick():
         group.set_max_velocity_scaling_factor(exec_vel)
-        group.set_joint_value_target([-0.6, 0.78, 0.06, 0.0, 1.5, -1.5, 0.06, 0.0])
-        group.go()
-        group.set_max_velocity_scaling_factor(exec_vel)
-        group.set_joint_value_target([-0.6, 0.78, 0.06, 0.0, 0.6, -0.78, 0.06, 0.0])
+        group.set_joint_value_target([-0.29, 0.21, 0, 0, 0.78, -0.78, 0.08, 0])
         group.go()
         rospy.sleep(sleep_time)
 
-        group.set_max_velocity_scaling_factor(exec_vel)
-        group.set_joint_value_target([-0.78, 0.78, 0.06, 0.0, 0.78, -0.78, 0.06, 0.0])
-        group.go()
-        rospy.sleep(sleep_time)
+    while True:
+        position_one()
+        position_two()
+        lower_pick()
