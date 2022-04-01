1eb3b98e2ba9c9a3964089c7adc8049add83ebb1
Adshymus <adshy91@gmail.com>
2022-04-01 14:20:01 +0200

    Cleanup to ignored files

--- Unity_env/Library/PackageCache/com.unity.ml-agents@1.6.0-preview/Runtime/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.ML-Agents.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.ML-Agents.Editor")]
-[assembly: InternalsVisibleTo("Unity.ML-Agents.Extensions")]
