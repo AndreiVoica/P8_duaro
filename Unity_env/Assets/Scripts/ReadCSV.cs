aff4865ada29ae8c52b5f52947f07c5aed72c114
Adshymus <adshy91@gmail.com>
2022-04-02 01:27:50 +0200

    Updated the way to read CSV file

--- Unity_env/Assets/Scripts/ReadCSV.cs
+++ Unity_env/Assets/Scripts/ReadCSV.cs
@@ -7,9 +7,6 @@ public class ReadCSV : MonoBehaviour
 {
     public string data_String;
     public string[] data_values;
-    public float jointAngle;
-    public float degJoint1L;
-    //public float jointAngle;
     void Start()
     {
         ReadCSVFile();
@@ -17,23 +14,17 @@ public class ReadCSV : MonoBehaviour
     }
     public void ReadCSVFile()
     {
-	var path = Directory.GetCurrentDirectory();
-	var filePath = Path.Combine(path, "bagfiles/test_1.csv");
+        var path = Directory.GetCurrentDirectory();
+        var filePath = Path.Combine(path, "bagfiles/test_1.csv"); 
 
         using (var strReader = new StreamReader(filePath))
         {
-            bool endOfFile = false;
-            while(!endOfFile)
+            strReader.ReadLine();
+            while (!strReader.EndOfStream)
             {
-                data_String = strReader.ReadLine ();
-                if(data_String == null)
-                {
-                    endOfFile = true;
-                    break;
-                }
+                data_String = strReader.ReadLine();
                 var data_values = data_String.Split(',');
-                float.TryParse(data_values[12], out float degJoint1L);
-                jointAngle = degJoint1L;
+                float.TryParse(data_values[12], out float jointAngle);
             }
         }
     }
