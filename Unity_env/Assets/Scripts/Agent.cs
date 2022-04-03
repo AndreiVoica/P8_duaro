690644d4d24c2b81877533439ee307f26bdf8b8e
Adshymus <adshy91@gmail.com>
2022-04-03 12:14:24 +0200

    MLAgent template

---
+++ Unity_env/Assets/Scripts/Agent.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Policies;
+using Unity.MLAgents.Actuators;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+class DuaroAgent : Agent
+{
+    private Library robot;
+
+    void Start()
+    {
+        robot = GetComponent<Library>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        robot.set_lower_joint_target(45, -45, 0, 0);
+        robot.set_upper_joint_target(-45, 45, 0, 0);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+    }
+}
