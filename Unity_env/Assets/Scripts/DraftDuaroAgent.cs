5aa70505fde2948ac7863671269082dbe88c58be
AndreiVoica <andreivoica24@gmail.com>
2022-04-21 13:51:16 +0200

    .

--- Unity_env/Assets/Scripts/DraftDuaroAgent.cs
+++ Unity_env/Assets/Scripts/DraftDuaroAgent.cs
@@ -22,14 +22,9 @@ public class DraftDuaroAgent : Agent
     public Transform yellow;
     public Transform blue;
     public Transform red;
-<<<<<<< HEAD
     private bool pickup_blue;
     private bool pickup_red;
     private bool pickup_rectangle;
-=======
-    public Transform rectangle;
-
->>>>>>> main
     private Control control;
 
     //Array to Store the items Position (Initial Row, Initial Column, Row Length, Column Length)
@@ -46,33 +41,16 @@ public class DraftDuaroAgent : Agent
                                     {1,1,1,1}, 
                                 };
 
-<<<<<<< HEAD
     // Boolean to check if there are items above in the rewards function                                
     private bool itemsAbove;
 
-=======
-    int[,] shapeBackup = new int[3,4]{ 
-                                    {0,0,0,0},
-                                    {1,0,0,1}, 
-                                    {1,1,1,1}, 
-                                };
-
-
-    // Boolean to check if there are items above in the rewards function                                
-    private bool itemsAbove;
-
-    private int checkAllDone;
-
->>>>>>> main
     //Max Number of Steps to be performed before the environment restarts
     [Tooltip("Max Environment Steps")] public int MaxEnvironmentSteps = 50000;
     private int m_resetTimer;
     //Max Number of Skills to be performed before the environment restarts
-    [Tooltip("Max Number of Skills")] public int MaxSkills = 6;
+    [Tooltip("Max Number of Skills")] public int MaxSkills = 10;
     private int m_resetSkill;
 
-    // Variable to store the cumulative Reward
-    float reward;
 
     public override void Initialize()
     {
@@ -84,7 +62,6 @@ public class DraftDuaroAgent : Agent
         // Reset cube positions:
         Vector3 rotationVector = new Vector3(0, 0, 0);
         Debug.Log("OnEpisodeBegin");
-<<<<<<< HEAD
         black.transform.localPosition = new Vector3(1.22f,0.789f,-1.3663f);
         black.transform.rotation = Quaternion.Euler(rotationVector);
         green.transform.localPosition = new Vector3(1.219f,0.898f,-1.32f);
@@ -92,32 +69,17 @@ public class DraftDuaroAgent : Agent
         Rectangle.transform.localPosition = new Vector3(1.22f,0.763f,-1.184f);
         Rectangle.transform.rotation = Quaternion.Euler(rotationVector);
         blue.transform.localPosition = new Vector3(1.219f,0.8231f,-1.267f);
-=======
-
-        blue.transform.localPosition = new Vector3(1.22300005f,0.823099971f,-1.296f);
->>>>>>> main
         blue.transform.rotation = Quaternion.Euler(rotationVector);
         yellow.transform.localPosition = new Vector3(1.2213f,0.8255f,-1.0598f);
         yellow.transform.rotation = Quaternion.Euler(rotationVector);
         red.transform.localPosition = new Vector3(1.221f,0.823f,-1.1674f);
         red.transform.rotation = Quaternion.Euler(rotationVector);
-<<<<<<< HEAD
         
         pickup_blue = false;
         pickup_red = false;
-=======
-        rectangle.transform.localPosition = new Vector3(1.22000003f,0.740999997f,-1.23800004f);
-        rectangle.transform.rotation = Quaternion.Euler(rotationVector);
->>>>>>> main
 
         m_resetTimer = 0;
         m_resetSkill = 0;
-
-        //shapeBackup.CopyTo(taskArray, 0);
-        taskArray = (int[,]) shapeBackup.Clone(); // make a copy
-        //taskArray = shapeBackup; 
-
-        //SetReward(0.0f);
         
     }
 
@@ -129,7 +91,6 @@ public class DraftDuaroAgent : Agent
         sensor.AddObservation(blue.position);
     }
 
-<<<<<<< HEAD
     // public override void OnActionReceived(ActionBuffers actionBuffers) //receives actions and assigns the reward
     // {      
     //     Debug.Log("OnActionReceived");
@@ -137,16 +98,6 @@ public class DraftDuaroAgent : Agent
     //     //MoveAgent(actionBuffers.DiscreteActions);
     //     // CHECK WHY USE THIS EVEN IN HEURISTIC MODE
     // }
-=======
-    public override void OnActionReceived(ActionBuffers actionBuffers) //receives actions and assigns the reward
-    {      
-         Debug.Log("OnActionReceived");
-        // Move the agent using the action.
-        MoveAgent(actionBuffers.DiscreteActions);
-        AgentRewards(actionBuffers.DiscreteActions);
-        // CHECK WHY APPLIES THIS EVEN IN HEURISTIC MODE
-    }
->>>>>>> main
 
 
     /// <summary>
@@ -157,16 +108,11 @@ public class DraftDuaroAgent : Agent
     {
         var decision = act[0];
        // decision = 0;
-<<<<<<< HEAD
         Debug.Log("test decision: " + decision);
-=======
-        //Debug.Log("Decision: " + decision);
->>>>>>> main
 
         switch (decision)
         {        
         case 0:
-<<<<<<< HEAD
             control.currentIndexL = 0;
             pickup_blue = true;
             control.PickBlackLower();
@@ -212,15 +158,6 @@ public class DraftDuaroAgent : Agent
         case 10: 
             control.currentIndexU = 0;
             control.PickWhiteUpper();
-=======
-            control.PickBlue();
-            break;
-        case 1:
-            control.PickRed();
-            break;
-        case 2:
-            control.PickWhite();
->>>>>>> main
             break;
         default:
             break;
@@ -236,7 +173,6 @@ public class DraftDuaroAgent : Agent
         var action = act[0];
 
         // Rewards
-<<<<<<< HEAD
         Debug.Log("action = " + action);
         if (action == 2)
         { 
@@ -247,57 +183,9 @@ public class DraftDuaroAgent : Agent
                 if (taskArray[itemsPosition[action,0] - 1, i] == 1)
                 {
                     itemsAbove = true; // There are some objects above this part
-=======
-        Debug.Log("Action = " + action);
-
-        // Check if there are items above
-        itemsAbove = false;
-        for (int i = itemsPosition[action,1]; i < itemsPosition[action,3]; i++)
-        {
-            if (taskArray[itemsPosition[action,0] - 1, i] == 1)
-            {
-                itemsAbove = true; // There are some objects above this part
-            }
-        }
-
-        if (taskArray[itemsPosition[action,0], itemsPosition[action,1]] == 1 && itemsAbove == false)
-        {
-            AddReward(1.0f);
-            
-            // Update items Matrix Space
-            for (int i = itemsPosition[action,1]; i < (itemsPosition[action,1] + itemsPosition[action,3]); i++)
-            {
-                for (int j = itemsPosition[action,0]; j < (itemsPosition[action,0] + itemsPosition[action,2]); j++)
-                {
-                    taskArray[j,i] = 0;
-                    //Debug.Log("Cleaning");
->>>>>>> main
                 }
             }
-            Debug.Log("Add Reward (+1)");
-        }
-        // Penalize choosing same action again
-        else if(taskArray[itemsPosition[action,0], itemsPosition[action,1]] == 0)
-        { 
-            AddReward(-1.0f);
-            Debug.Log("Add Negative Reward - Choosing the same action (-1)");
-        }
-        // Penalize picking up the rectangle while there is something above
-        if(itemsAbove == true)
-        { 
-            AddReward(-1.0f);
-            Debug.Log("Add Negative Reward - There is something above (-1)");
-        } 
 
-        //*********************************************************
-        // TO BE DONE: (It is not scalable, just for training test)
-        checkAllDone = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            checkAllDone = checkAllDone + taskArray[2,i];
-        }
-
-<<<<<<< HEAD
             if (taskArray[itemsPosition[action,0], itemsPosition[action,1]] == 1 && itemsAbove == false)
             {
                 AddReward(1.0f);
@@ -325,35 +213,6 @@ public class DraftDuaroAgent : Agent
                 Debug.Log("Add Negative Reward - There is something above");
             }
         }  
-=======
-        if (checkAllDone == 0)
-        {
-            AddReward(5.0f);
-            Debug.Log("TASK COMPLETED +5!");
-            EndEpisode();
-        }
-        //********************************************************* 
-
-        // CHECK IF taskArray ONLY CONTAINS ZEROS, GIVE A HIGH REWARD AND RESET 
-        // TO DO BETTER:
-
-        // var allElementsAreZero = taskArray.All(o => o == 0);
-        // if (allElementsAreZero == true)
-        // {
-        //     AddReward(10.0f);
-        //     Debug.Log("TASK COMPLETED +10!");
-        //     EndEpisode();
-        // }
-
-        // foreach (int a in taskArray) 
-        //   {
-        //       Debug.Log(a);
-        //   }
-        
-        // Update and show Cumulative Reward
-        reward = GetCumulativeReward();        
-        Debug.Log("CumulativeReward: " + reward);
->>>>>>> main
     }
 
 
@@ -383,7 +242,6 @@ public class DraftDuaroAgent : Agent
             MoveAgent(actionsOut.DiscreteActions);
             AgentRewards(actionsOut.DiscreteActions);
         }
-<<<<<<< HEAD
         else if(Input.GetKey(KeyCode.Keypad1) && control.currentIndexU >= control.jointAnglesU.Count)
         {
             discreteActionsOut[0] = 3;
@@ -441,17 +299,12 @@ public class DraftDuaroAgent : Agent
             AgentRewards(actionsOut.DiscreteActions);
         }
         Debug.Log("discreteActionsOut = " + discreteActionsOut[0]);
-=======
-        // Debug.Log("discreteActionsOut = " + discreteActionsOut[0]);
->>>>>>> main
     }	 
 
 
     void FixedUpdate()
     {
-        // Update Cumulative Reward
-        reward = GetCumulativeReward();
-
+        Debug.Log("Fixed Update");
         m_resetTimer += 1; // Add +1 to Steps Counter
         if (m_resetTimer >= MaxEnvironmentSteps && MaxEnvironmentSteps > 0)
         {
